d83da361175f6efab789fac473c0820f9ec32a07
Ilia Chernyshevskiii <106028920+kyxtya023@users.noreply.github.com>
2023-05-25 13:12:04 +0300

    Add files via upload

---
+++ dist/_nuxt/food.7b519c2e.js
@@ -0,0 +1,1 @@
+import{f as o,a as e,o as t}from"./entry.e1738a70.js";const c={};function r(s,a,n,f,p,_){return t(),e("div")}const i=o(c,[["render",r]]);export{i as default};
---
+++ dist/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1685009336763}
